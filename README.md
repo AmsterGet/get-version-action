39a49a0b9cfd60fce0b43af7daf49473c30a59f2
amsterget <amsterget@gmail.com>
2020-05-27 10:22:23 +0300

    Action test

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # get-version-action
+
+# Get version type javascript action
+
+This action calculates version type according to event.
+
+## Outputs
+
+### `version-type`
+
+The version type.
+
+## Example usage
+
+uses: amsterget/get-version-action@v1
---
+++ index.js
@@ -0,0 +1,18 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+    // const nameToGreet = core.getInput('who-to-greet');
+    console.log('-----CONTEXT-----');
+    console.log(github.context);
+    console.log('-----CONTEXT-----');
+    console.log('-----EVENT-----');
+    console.log(github.event);
+    console.log('-----EVENT-----');
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`PAYLOAD ${payload}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("version-type", 'patch');
+} catch (error) {
+    core.setFailed(error.message);
+}
